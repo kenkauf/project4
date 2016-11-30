edaf66a89cec00f684232ad449a713647351c245
kenkauf <kenkaufman3@gmail.com>
2016-11-30 13:14:06 -0500

    fixed snake boundary issue

--- snake.py
+++ snake.py
@@ -1,103 +1,86 @@
-import pygame
-from random import randint
-
 print("\nKennedy Kaufman\n\n61371023")
 
-BLACK = (0, 0, 0)
+#import modles
+import pygame
+import random
+
+#define colors
 WHITE = (225, 225, 225)
-RED = (255,0,0)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
-snake_body_w = 15
-snake_body_h = 15
-snake_body_space = 3
+#define sizes of sprites
+pieceSize = 10
+sizeOfWorm = 10
+obstacleSize = 10
 
-x_change = snake_body_w + snake_body_space
-y_change = 0
+class Worm:
 
-score = 0
+    def __init__(self, surface):
+        self.surface = surface
+        self.x = surface.get_width() / 2
+        self.y = surface.get_width() / 2
+        # http://www.pygame.org/docs/ref/surface.html
+        self.length = pieceSize
+        self.grow_to = pieceSize*2
+        self.vx = 0
+        self.vy = -pieceSize
+        self.body = []
+        self.head = None
+        self.crashed = False
+        self.color = WHITE
 
-class Snake(pygame.sprite.Sprite):
+    def event(self, event):
+        if event.key == pygame.K_UP:
+            if self.vy == pieceSize: return
+            self.vx = 0
+            self.vy = -pieceSize
+        elif event.key == pygame.K_DOWN:
+            if self.vy == -pieceSize: return
+            self.vx = 0
+            self.vy = pieceSize
+        elif event.key == pygame.K_LEFT:
+            if self.vx == pieceSize: return
+            self.vx = -pieceSize
+            self.vy = 0
+        elif event.key == pygame.K_RIGHT:
+            if self.vx == -pieceSize: return
+            self.vx = pieceSize
+            self.vy = 0
 
-	def __init__(self, x, y):
-		pygame.sprite.Sprite.__init__(self)
-		self.pos = [200, 220]
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
 
-		self.image = pygame.Surface([snake_body_w, snake_body_h])
-		self.image.fill(WHITE)
+        if (self.x, self.y) in self.body:
+            self.crashed = True
 
-		self.rect = self.image.get_rect()
-		self.rect.x = x
-		self.rect.y = y
+        self.body.insert(0, (self.x, self.y))
+        self.head = pygame.Rect(self.x, self.y, sizeOfWorm, sizeOfWorm)
 
-	def out_of_bounds(self):
+        if (self.grow_to > self.length):
+            self.length += pieceSize
 
-		if self.rect.x > 600 or self.rect.y > 800 or self.rect.x < 0 or self.rect.y < 0:
-			game_over = True
-			print("Knows OOB")
+        if (len(self.body) > self.grow_to):
+            pop = self.body.pop(-1)
+            pygame.draw.rect(self.surface, BLACK, (pop[0], pop[1], sizeOfWorm, sizeOfWorm), 0)
 
-	def update(self):
+        if (len(self.body) > self.length):
+            pop = self.body.pop(-1)
+            pygame.draw.rect(self.surface, BLACK, (pop[0], pop[1], sizeOfWorm, sizeOfWorm), 0)
 
-		self.pos[0] = self.rect.x
-		self.pos[1] = self.rect.y 
+    def draw(self):
+        pygame.draw.rect(self.surface, self.color, (self.head))
+        x, y = self.body[-1]
+        pygame.draw.rect(self.surface, BLACK, (x, y, sizeOfWorm, sizeOfWorm), 0)
 
-		if self.out_of_bounds():
-			game_over=True
+    def position(self):
+        return self.x, self.y
 
-FoodX = 10*randint(0,800/10-1)
-FoodY = 10*randint(0,600/10-1)
-Food = pygame.Surface((10,10))
-Food.fill(RED)
-
-pygame.init()
-screen = pygame.display.set_mode([800, 600])
-pygame.display.set_caption('Snake')
-allspriteslist = pygame.sprite.Group()
-
-snake_body = []
-for i in range(3):
-	x = 250-(snake_body_w+snake_body_space)*i
-	y = 30
-	body = Snake(x, y)
-	snake_body.append(body)
-	allspriteslist.add(body)  #REFERENCE: http://stackoverflow.com/questions/13851051/how-to-use-sprite-groups-in-pygame
-
-clock = pygame.time.Clock()
-game_over = False
-
-while not game_over:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			game_over = True
-
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				x_change = (snake_body_w + snake_body_space) * -1
-				y_change = 0
-			if event.key == pygame.K_RIGHT:
-				x_change = (snake_body_w + snake_body_space)
-				y_change = 0
-			if event.key == pygame.K_UP:
-				x_change = 0
-				y_change = (snake_body_h + snake_body_space) * -1
-			if event.key == pygame.K_DOWN:
-				x_change = 0
-				y_change = (snake_body_h + snake_body_space)
-
-	old_body = snake_body.pop()
-	allspriteslist.remove(old_body)
-
-	x = snake_body[0].rect.x + x_change
-	y = snake_body[0].rect.y + y_change
-	body = Snake(x, y)
-
-	snake_body.insert(0, body)
-	allspriteslist.add(body)
-
-	screen.fill(BLACK)
-	allspriteslist.draw(screen)
-	clock.tick(5)
-	screen.blit(Food, (FoodX, FoodY))
-	pygame.display.flip()          # Reference: http://www.pygame.org/docs/ref/display.html
+    def eat(self):
+        self.grow_to += pieceSize
 
 
-pygame.quit()
+
+    pygame.display.flip()
+    clock.tick(30)
