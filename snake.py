a303a34ed0bf3e88315481f37dc9c84ca83eb4e2
kenkauf <kenkaufman3@gmail.com>
2016-11-29 21:21:08 -0500

    finished making body and add to sprite list

--- snake.py
+++ snake.py
@@ -7,8 +7,8 @@ snake_body_w = 15
 snake_body_h = 15
 snake_body_space = 3
 
-x1 = snake_body_w + snake_body_space
-y2 = 0
+x_change = snake_body_w + snake_body_space
+y_change = 0
 
 class Snake(pygame.sprite.Sprite):
 
@@ -26,3 +26,12 @@ pygame.init()
 screen = pygame.display.set_mode([800, 600])
 pygame.display.set_caption('Snake')
 allspriteslist = pygame.sprite.Group()
+
+snake_body = []
+for i in range(15):
+	x = 250-(snake_body_w+snake_body_space)*i
+	y = 30
+	body = Snake(x, y)
+	snake_body.append(body)
+	allspriteslist.add(body)  #REFERENCE: http://stackoverflow.com/questions/13851051/how-to-use-sprite-groups-in-pygame
+
