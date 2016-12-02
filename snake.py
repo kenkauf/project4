a04041bf69461aa4d2fb11c006ea9eac797449e9
kenkauf <kenkaufman3@gmail.com>
2016-12-02 16:15:46 -0500

    commit for points

--- snake.py
+++ snake.py
@@ -1,12 +1,12 @@
 print("\nKennedy Kaufman\n61371023\n")
 
-#  TURN ON SOUND, super cool background music will help increase scores, its science
+#  DON'T FORGET to turn your sound on, super cool background music will help increase scores, its science.
 
 #  Snake Game for SI 206 Project 4
 #  Use arrow keys to move snake around
 #  Eat the red apples and increase your score
 #  Don't eat yourself or go outside the boundries
-#  final score will be output
+#  Final score will be outputed in terminal or command line
 
 #  import modles
 import pygame
